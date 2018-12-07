c878dfd63720ae8c4a4f0cbcc46838c2eda6538b
root <root@C1.local>
2018-12-07 08:01:41 +0000

    Replaced script with correct one

---
+++ rng.sh
@@ -0,0 +1,4 @@
+#!/bin/ash
+
+dd if=/dev/urandom bs=4 count=16 status=none | od -A none -t u4
+#echo "hello";
--- socatstartup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/ash
-
- socat -v -v tcp-listen:8080,fork,reuseaddr exec:/bin/rng.sh 1>&/dev/null
